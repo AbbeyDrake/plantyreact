c0d60c09c064306208265c07786f934c25e8eba2
Sarah Kielian <kielian.sarah@gmail.com>
2022-02-02 14:17:04 -0700

    added planty images and header

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,29 @@
+import React, { Component }  from 'react';
+import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
+
+class Header extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Jumbotron fluid>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col">
+                                <h1>Planty</h1>
+                                <h2>In Plants We Trust</h2>
+                            </div>
+                        </div>
+                    </div>
+                </Jumbotron>
+
+                <Navbar dark sticky="top">
+                    <div className="container">
+                        <NavbarBrand href="/">Planty</NavbarBrand>
+                    </div>
+                </Navbar>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Header;
