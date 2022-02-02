01ad58ec1dcf887644c0dd6724e83f9e2086a8d9
Abbey Drake <drakeam1189@gmail.com>
2022-02-02 16:52:06 -0700

    installed reactstrap

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@
 import './App.css';
 import { Route, Switch, BrowserRouter} from 'react-router-dom';
 import Footer from './components/FooterComponent';
+import Header from './components/HeaderComponent';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        {/* <Header/> */}
+        <Header/>
           <Switch>
               
           </Switch>
--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -2,6 +2,7 @@ import React, { Component }  from 'react';
 import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
 
 class Header extends Component {
+    
     render() {
         return (
             <React.Fragment>
