c3110191e23d0c0a4aeedf3a04a8ab2a05024252
Abbey Drake <drakeam1189@gmail.com>
2022-02-07 16:47:12 -0700

    adding localshopsdisplaycomponent

--- src/components/LocalShopsComponent.js
+++ src/components/LocalShopsComponent.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import { Card, CardBody, CardHeader, Media} from 'reactstrap';
+import { Card, CardBody, CardHeader, Row, Col} from 'reactstrap';
 import NewsletterComponent from './NewsletterComponent';
 import Iframe from 'react-iframe';
+import LocalShops from './LocalShopsDisplayComponent';
 
 function LocalShopsComponent(props) {
   return (
     <div>
         <div className="container m-3 my-5">
             <h1>Local Shops</h1>
-            <div className="row">
-                <div className="col-sm-6 mt-3">
+            <Row className="row">
+                <Col className="col-sm-6 mt-3">
                     <h3>Find Your Local Green Spots!</h3>
                     <h5>Denver, Colorado</h5>
                     <p>Looking for unique, local plant shops and nurseries to grow your plant family? Denver is home to many plant havens where you can browse extensive collections, take classes and learn more about plant care, and connect with the community through events. </p>
                     <div  className="form-row justify-content-center">
                         <a type="button" href="#localCarousel" className="btn btn-sm btn-dark mt-2 mb-3">See More</a>
                     </div>
-                </div>
-                <div className="col mb-4">
+                </Col>
+                <Col className="col mb-4">
 
                     <Card className="card-cascade-narrower">
                         <CardHeader className="align-self-center mt-2">
@@ -37,10 +38,11 @@ function LocalShopsComponent(props) {
                                 </div>
                         </CardBody>
                     </Card>
-                </div>
-            </div>
+                </Col>
+            </Row>
         </div>
         <hr></hr>
+        <LocalShops></LocalShops>
         <NewsletterComponent></NewsletterComponent>
     </div>
   );
---
+++ src/components/LocalShopsList.js
@@ -0,0 +1,50 @@
+export const LOCALSHOPS = [
+    {
+        id:0,
+        name: "Birdsall",
+        image: "/assets/birdsall.png",
+        description: "Birdsall is a locally-owned family business run by women with two locations in Denver. They are known best for their massive selection of outdoor containers and have an ever-rotating selection of houseplants. Click here to see upcoming events, such as their holiday wreath making workshop!",
+        website: "https://birdsallgarden.com/blogs/events"
+    },
+
+    {
+        id:1,
+        name: "Bronze Petal",
+        image: "/assets/bronzepetal.png",
+        description: "The Bronze Petal, Ltd. is a Denver, Colorado based Aroid small business that specializes in the sale and education of common, uncommon, rar in the Mile High City. These aroids are classified as ultra-rare or extraordinarily difficult to obtain, and The Bronze Petal strives to carry these heavily sought after beauties as long as they are available from reputable sources. Check out some of their upcoming events here",
+        website: "https://www.thebronzepetal.com/events"
+    },
+
+    {
+        id:2,
+        name: "City Floral",
+        image: "/assets/cityfloral.png",
+        description: "Established in 1911, City Floral Garden Center is the oldest remaining greenhouse in Denver. The Lammermann family founded a greenhouse for cut flower production and Mr. Lammerman would ride his bicycle down Colfax Avenue (then a dirt road) and deliver cut violets to the Brown Palace. The greenhouse has grown since then, and the massive garden center has an extensive selection of plant and gardening inventory and tools.",
+        website: ""
+    },
+
+    {
+        id:3,
+        name: "Planted",
+        image: "/assets/planted.png",
+        description: "Planted is a full-service plant shop located in Denver near Sloan's Lake owned by Colorado-born Luke Huss. 'Living Green' is the Planted business model and manifesto - not only by being surrounded by living greens - but the lifestyle of being green & upcycling. They make, use and sell reclaimed wood plant displays, containers, wallscapes, and much more.",
+        website: ""
+    },
+
+    {
+        id:4,
+        name: "Reroot",
+        image: "/assets/reroot.png",
+        description: "ReRoot, located in the Rino Arts District in Denver, began with an urge to re-connect people to nature by creating immersive, mossy, dreamy art installations and sprouted into a retail shop, pretty much overnight. They became not only a retail store but a botanical community-focused creative collective, filled with all things green. For them, it's about cultivating our special relationship with plants and sharing that connection with others. They strive to be stewards of the earth and nurture their community near and far as they find their connection with nature.",
+        website: ""
+    },
+
+    {
+        id:5,
+        name: "Green Lady Gardens",
+        image: "/assets/GLG.png",
+        description: "Green Lady Gardens is a local woman-owned business that creates a personalized and educational plant shopping experience. GLG is a committed member of the Santa Fe Arts District community and boasts a colorful shop full of joy and inspiration. GLG is committed to socially responsible and culturally conscious business practices that show respect for the Latino community they draw inspiration from. See some of these business practices",
+        website: ""
+    },
+
+]
