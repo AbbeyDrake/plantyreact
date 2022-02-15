2c14789cb4acc2f7de5aa188d2d6ea15d0e15371
Abbey Drake <drakeam1189@gmail.com>
2022-02-15 09:31:11 -0700

    adding cards to home component

--- src/components/HomeCardsList.js
+++ src/components/HomeCardsList.js
@@ -21,7 +21,31 @@ export const HOMECARDSLIST = [
         name: "plans in office",
         image: "https://www.goodnet.org/photos/620x0/31877_hd.jpg",
         description: "Interacting with nature also helps our bodies heal. Researchers found that rooms with plants have less dust and mold than rooms without any foliage. Leaves and other parts  of the plants act as natural filters to catch allergens and other airborne particles. Common low-light houseplants like Chinese evergreen or the peace lily can do the job. Violets and other plants with textured leaves might be even better trappers"
-    }
+    },
+
+    {
+        id:3,
+        title: "Gardening Therapy",
+        name: "",
+        image: "",
+        description: "Gardening is the greatest tonic and therapy a human being can have. Even if you have only a tiny piece of earth, you can create something beautiful, which we all have a great need for. If we begin by respecting plants, it's inevitable we'll respect people. -Audrey Hepburn"
+    },
+
+    {
+        id:4,
+        title: "How Plants Heal the Soul",
+        name: "woman and plant",
+        image: "https://www.guyabouthome.com/wp-content/uploads/2021/04/woman-with-her-eyes-closed-near-a-fern-.jpeg",
+        description: "The use of medicinal plants in healing is therefore a time-honored cultural practice of Indigenous people observed in ceremonies, prayer, and medicine. Their use in  ceremonies and prayers enacts the power of the spiritual realm that elicits the Creator's intervention in healing. Plants remind us that Mother Earth, our first teacher, has provided us with the tools necessary to heal physically, mentally, emotionally, and spiritually."
+    },
+
+    {
+        id:5,
+        title: "Plants Repel Bad Vibes and so Much More!",
+        name: "woman and plant",
+        image: "https://bloomscape.com/wp-content/uploads/2020/07/Bloomscape_Spring20_106-1024x683.jpg?ver=258077g",
+        description: "House plants engage the senses, especially touch and smell. Being engaged in the moment can help prevent anxiety, depression and insomnia."
+    },
 
 
 ]
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -5,43 +5,43 @@ import {Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
 import { HOMEIMAGESLIST } from './HomeImagesList';
 import { HOMECARDSLIST } from './HomeCardsList';
 
-// function RenderCard(){
-//     return(
-//         HOMECARDSLIST.map((homeCard)=>{
-//           return(
-//                 <Card>
-//                     <CardImg src={homeCard.image} alt = {homeCard.name}/>
-//                     <CardBody>
-//                       <CardTitle>{homeCard.title}</CardTitle>
-//                       <CardText{homeCard.description}></CardText>
-//                     </CardBody>
-//                 </Card>
-//           );
-//       })
-//     );
-// } 
+function RenderCard(){
+    return(
+      <div>
+        {HOMECARDSLIST.map(homeCard => {
+          return(
+                  
+                  <Card>
+                      <h2>{homeCard.title}</h2>
+                      <CardImg src={homeCard.image} alt = {homeCard.name}/>
+                      <CardBody>
+                        {homeCard.description}
+                      </CardBody>
+                  </Card>
+                 
+          );
+      })}
+       </div>
+    );
+} 
 
 
 function HomeComponent() {
   return (
   <div>
      <div className="container">
-        <div className="row">
-            <div className="col-sm-10 offset-1">
-                <h1>Plant of the Day</h1>
-                <p>Planty, invites Plant Lovers around the world to experience the ultimate Planty experience. Join us
-                    as we share our love for all things house plants and sustainability! The Plant of the Day, is a
-                    seven day series of your
-                    favorite plants and all their care needs.
-                    Here at Planty, we believe that plants add value to our world in more ways that we know. Plants
-                    clean our air, brightening our mood, and uplifting any room or environment. They also help cultivate
-                    sustainable communities worldwide! When you live life the Planty way, you can't go wrong.
-                </p>
-                <h3>Adorn your Planty Space today!</h3>
-            </div>
-        </div>
-    </div>
+          <div className="row">
+              <div className="col-sm-8 offset-2">
+                  <h1>Plant of the Day</h1>
+                  <p>Planty, invites Plant Lovers around the world to experience the ultimate Planty experience. Join us as we share our love for all things house plants and sustainability! The Plant of the Day, is a seven day series of your favorite plants and all their care needs. Here at Planty, we believe that plants add value to our world in more ways that we know. Plants clean our air, brightening our mood, and uplifting any room or environment. They also help cultivate sustainable communities worldwide! When you live life the Planty way, you can't go wrong.
+                  </p>
+                  <h3>Adorn your Planty Space today!</h3>
+              </div>
+          </div>
+      </div>
+
     <hr></hr>
+
       <div className='container'>
         <div className='row'>
           <div className='col-sm-8 offset-2'>
@@ -58,10 +58,12 @@ function HomeComponent() {
             </Carousel>
           </div>
         </div>
+      </div>
+      <div className="container">
         <div className="row">
-          {/* <div className='col'>
+          <div className='col-sm-6'>
             <RenderCard></RenderCard>
-          </div> */}
+          </div>
         </div>
       </div>
   </div>
