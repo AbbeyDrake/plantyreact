10e0c90b0bf16d93f96da28756c389a6f075fcc7
Abbey Drake <drakeam1189@gmail.com>
2022-02-14 17:01:37 -0700

    home page

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,8 +1,50 @@
-import React from 'react';
+import React, { Component }  from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import { HOMEIMAGESLIST } from './HomeImagesList';
 
 
-function HomeComponent(props) {
-  return <div></div>;
+function HomeComponent() {
+  return (
+  <div>
+     <div className="container mt-3">
+        <div className="row">
+            <div className="col-sm-8 offset-2">
+                <h1>Plant of the Day</h1>
+                <p>Planty, invites Plant Lovers around the world to experience the ultimate Planty experience. Join us
+                    as we share our love for all things house plants and sustainability! The Plant of the Day, is a
+                    seven day series of your
+                    favorite plants and all their care needs.
+                    Here at Planty, we believe that plants add value to our world in more ways that we know. Plants
+                    clean our air, brightening our mood, and uplifting any room or environment. They also help cultivate
+                    sustainable communities worldwide! When you live life the Planty way, you can't go wrong.
+                </p>
+                <h3>Adorn your Planty Space today!</h3>
+            </div>
+        </div>
+    </div>
+    <hr></hr>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-sm-8 offset-2'>
+            <Carousel autoPlay showArrows={true} infiniteLoop>
+                {HOMEIMAGESLIST.map((homeimage)=>{
+                    return(
+                            <div key = {homeimage.id}>
+                                <h1>{homeimage.title}</h1>
+                                <img className = "carousel-image" src={homeimage.image}/>
+                            </div>
+                      
+                    )
+                })}   
+          </Carousel>
+          </div>
+        </div>
+      </div>
+    <div>
+
+    </div>
+  </div>);
 }
 
 
---
+++ src/components/HomeImagesList.js
@@ -0,0 +1,65 @@
+export const HOMEIMAGESLIST = [
+    {
+        id:0,
+        title: "Plant of the Day",
+        name: "Plant of the Day",
+        image: "/assets/images/Plant of the Day.jpeg",
+    },
+
+    {
+        id:1,
+        title: "Sunday: Prayer Plant",
+        name: "Prayer Plant",
+        image: "/assets/images/PrayerPlant.png",
+        
+    },
+
+    {
+        id:2,
+        title: "Monday: Fiddle Leaf Fig",
+        name: "Fiddle Leaf Fig",
+        image: "/assets/images/FiddleLeafFig.png",
+        
+    },
+
+   
+    {
+        id:3,
+        title: "Tuesday: Monstera Plant",
+        name: "Monstera",
+        image: "/assets/images//Monstera.png",
+        
+    },
+
+    {
+        id:4,
+        title: "Wednesday: Snake Plant",
+        name: "Snake Plant",
+        image: "/assets/images/Snake Plant.png",
+        
+    },
+
+    {
+        id:5,
+        title: "Thursday: Golden Pothos",
+        name: "Golden Pothos",
+        image: "/assets/images//GoldenPothos_Hang.png",
+        
+    },
+
+    {
+        id:6,
+        title: "Friday: Venus Fly Trap",
+        name: "Venus Fly Trap",
+        image: "/assets/images/Venus.png",
+        
+    },
+
+    {
+        id:7,
+        title: "Saturday: Birds of Paradise",
+        name: "Birds of Paradise",
+        image: "/assets/images/BirdsofParadise.png",
+        
+    }
+]
